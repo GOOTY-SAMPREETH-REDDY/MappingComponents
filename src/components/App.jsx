d1b031ce3071bcb5574ee7d28e698740d6a8118b
GOOTY-SAMPREETH-REDDY <109329775+GOOTY-SAMPREETH-REDDY@users.noreply.github.com>
2023-06-19 12:54:50 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Card from "./Card";
+import contacts from "../contacts";
+function createcard(cont) {
+  return (
+    <Card
+      name={cont.name}
+      key={cont.id}
+      img={cont.imgURL}
+      tel={cont.phone}
+      email={cont.email}
+    />
+  );
+}
+
+function App() {
+  return (
+    <div>
+      {contacts.map(createcard)};<h1 className="heading">My Contacts</h1>
+      {/*<Card
+        name={contacts[0].name}
+        img={contacts[0].imgURL}
+        tel={contacts[0].phone}
+        email={contacts[0].email}
+      />
+      <Card
+        name={contacts[1].name}
+        img={contacts[1].imgURL}
+        tel={contacts[1].phone}
+        email={contacts[1].email}
+      />
+      <Card
+        name={contacts[2].name}
+        img={contacts[2].imgURL}
+        tel={contacts[2].phone}
+        email={contacts[2].email}
+      />*/}
+    </div>
+  );
+}
+
+export default App;
